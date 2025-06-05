5dd76a55389a52bfa279087c227ef1731f2ce30f
phoebe <phoebe.engelmann@t-online.de>
2025-06-05 11:17:03 +0200

    test2

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-
-
 @app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
